457e261c2c85afdb48b594f46fa77e00fbf3d32f
hazael <hazaelx2r@gmail.com>
2020-03-06 09:24:28 -0400

    Ready for deployment to heroku.

--- ministry_journal/settings.py
+++ ministry_journal/settings.py
@@ -147,9 +147,14 @@ if os.getcwd() == "/app":
     # Allow all host headers.
     ALLOWED_HOSTS = ['*']
 
-    # Static asset configuration
-    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-    STATIC_ROOT = 'staticfiles'
+    BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+    # Static files (CSS, JavaScript, Images)
+    # https://docs.djangoproject.com/en/1.9/howto/static-files/
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    STATIC_URL = '/static/'
+
+    # Extra places for collectstatic to find static files.
     STATICFILES_DIRS = (
         os.path.join(BASE_DIR, 'static'),
     )
